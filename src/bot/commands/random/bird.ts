1237ae56afc04bc92f4c81d6c9bb4932fc3ccb3b
ROSS HALL <ROSHALL@uat.edu>
2019-04-05 15:57:17 -0700

    Create bird command

--- src/bot/commands/random/bird.ts
+++ src/bot/commands/random/bird.ts
@@ -1,11 +1,7 @@
 import { Command, Input, Listener } from '@api';
-import * as request from 'request';
 import { Message } from 'discord.js';
 import * as fs from 'fs';
 import { Emoji } from '@bot/libraries/emoji';
-import { Url } from 'url';
-import { pathMatch } from 'tough-cookie';
-const entities = require("html-entities").AllHtmlEntities;
 
 export class Bird extends Command {
     constructor() {
@@ -17,28 +13,15 @@ export class Bird extends Command {
     }
 
     async execute(input: Input) {
-        let dirPath : string = '/public/images/birds';
-        let images = fs.readdir(dirPath, 'binary', (err, files) => {
-            if(err){
-                console.log(files.join)
-            }
-        });
-        let rnd = Math.floor(Math.random() * images.length);
-
-
-        fs.readFile(dirPath, "binary" );
+       
+        let dirPath = fs.readdirSync(pub('/images/birds'));
+        let rndImage = Math.floor(Math.random() * dirPath.length);
+        let buffer = fs.readFileSync(dirPath[rndImage]);
         let message = await input.channel.send(`${Emoji.LOADING}  Fetching image...`) as Message;
 
-        input.channel.send({
-            embed: {
-                color: 3447003,
-                image:
-                {
+        await input.channel.send({ file: [await buffer] });
 
-                }
-            }
-        });
-
+        message.delete();
     };
 }
 
