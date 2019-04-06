3d0301924cc4e99f464952540a8cd3d0b0f47be1
ROSS HALL <ROSHALL@uat.edu>
2019-04-05 18:14:54 -0700

    Create dad joke command

--- src/bot/commands/random/dad-jokes.ts
+++ src/bot/commands/random/dad-jokes.ts
@@ -3,6 +3,8 @@ import * as request from 'request';
 import { Message } from 'discord.js';
 import { Emoji } from '@bot/libraries/emoji';
 import { Url } from 'url';
+import { monitorEventLoopDelay } from 'perf_hooks';
+import { CookieJar } from 'tough-cookie';
 const entities = require("html-entities").AllHtmlEntities;
 
 export class DadJokes extends Command {
@@ -19,36 +21,40 @@ export class DadJokes extends Command {
         let url = `https://icanhazdadjoke.com/`;
 
         console.log(url);
+
         let message = await input.channel.send(`${Emoji.LOADING}  Fetching image...`) as Message;
+        let headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'Ember bot'
+            //'X-API-KEY': apiKey,
+        }
 
         //Fetch from API
-        request(url, async (err, response, body) => {
-            //Handle HTTP errors
+        request({url, headers}, async (err, response, body) => {
+            // Handle HTTP errors
             if (err) {
+                this.getLogger().error(err);
                 await message.edit(`${Emoji.ERROR}  Failed to get image, try again later.`);
                 return;
             }
 
-            console.log(body);
-
-            //Parse the body
+            // Parse the body
             let parsed = (<ApiResponse>JSON.parse(body));
 
             console.log(parsed.joke);
             console.log(parsed.id);
-            // Delete the loading message
-            try { await message.delete(); } catch(err) {}
 
-            input.channel.send({
+            // Delete the message if it can be deleted
+            message.deleteAfter(0);
+
+            // Send joke
+            await input.channel.send({
                 embed: {
                     color: 3447003,
-                    image:
-                    {
-                        url: `https://icanhazdadjoke.com/j/${parsed.id}.png`
-                    }
-            }
+                    title: `**Dad Joke**` ,
+                    description: parsed.joke
+                }
             });
-
         });
     }
 }
