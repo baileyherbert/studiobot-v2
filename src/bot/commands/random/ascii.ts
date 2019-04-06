fd484c45a81f55d0138bdbe46d1a7acd9d5120b3
Bailey Herbert <bailey.herbert@hotmail.com>
2019-04-05 21:02:06 -0700

    Fix ascii art showing blank and/or unescaped lines

--- src/bot/commands/random/ascii.ts
+++ src/bot/commands/random/ascii.ts
@@ -1,6 +1,6 @@
 import { Command, Input } from '@api';
 
-const ascii = readPublicFile('random/ascii.txt').split(/\r?\n\r?\n/);
+const ascii = readPublicFile('random/ascii.txt').split(/\r?\n=+\r?\n/);
 
 export class asciiArt extends Command {
     constructor() {
@@ -9,11 +9,11 @@ export class asciiArt extends Command {
             description: 'Returns a random ASCII art from the text file.'
         });
     }
-    
-    async execute(input: Input) {
-        let index = _.random(0, ascii.length - 1);
-        let line = ascii[index];
 
-        await input.channel.send('```\n' + line + '\n```'); 
+    async execute(input: Input) {
+        let index = _.random(1, ascii.length - 1);
+        let line = ascii[index].replace(/^(\r?\n)+/, '').replace(/`/g, '\\`');
+
+        await input.channel.send('```\n' + line + '\n```');
     }
 }
