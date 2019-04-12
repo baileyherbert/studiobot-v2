581fedfe0a60d61ae2629299696414cbabba5fbe
RSingh39 <rishabh_singh39@yahoo.com>
2019-04-12 16:36:44 -0700

    Updated the slot machine and polished code

--- src/bot/commands/games/slots.ts
+++ src/bot/commands/games/slots.ts
@@ -1,26 +1,44 @@
 import { Command, Input } from '@api';
+import { RichEmbed } from 'discord.js';
+import { Economy } from '@bot/libraries/economy';
 
 export class Ping extends Command {
     constructor() {
         super({
-            name: 'Slot Machine',
+            name: 'slot',
             description: 'Creates a slot machine game for the users.'
         });
     }
 
-    execute(input: Input) {
 
-        let emojis = ["🍓", "🍑", "🍀"];
+    async execute(input: Input) {
+    //"🍒", "⚡", "💸", "💰", "🤑","🍓", "🍑",
+        let emojis = ["🍀", "🌸", "🍄"];
+        let slot1 = Math.floor((Math.random() * emojis.length));
+        let slot2 = Math.floor((Math.random() * emojis.length));
+        let slot3 = Math.floor((Math.random() * emojis.length));
 
-        emojis[0]; "🍓"
-        emojis[1]; "🍑" 
-        emojis[2]; "🍀"
+        //if won then display that the user has won
+        if(emojis[slot1] === emojis[slot2] && emojis[slot1] === emojis[slot3]){
+            let wEmbed = new RichEmbed()
+                .setFooter("Comgrats, You've Won!")
+                .setTitle(':slot_machine: SLOT MACHINE :slot_machine:')
+                .addField('Result!', emojis[slot1] + emojis[slot2] + emojis[slot3]);
+                
+            await input.channel.send(wEmbed);
 
-        let slotA = emojis[Math.floor(Math.random() * emojis.length)];
-        let slotB = emojis[Math.floor(Math.random() * emojis.length)];
-        let slotC = emojis[Math.floor(Math.random() * emojis.length)];
+            //needs to give use an amount if they win
+            Economy.addBalance(input.member, 50);
+            await input.channel.send(`:moneybag:  ${input.member} received **$${50}**.`);
+        }
 
+        //if not then output you've lost 
+        else{
+            let wEmbed = new RichEmbed()
+                .setFooter("You've Lost, Try again?")
+                .setTitle(':slot_machine: SLOT MACHINE :slot_machine:')
+                .addField('Result', emojis[slot1] + emojis[slot2] + emojis[slot3]);
+            input.channel.send(wEmbed);
+        }
     }
-
-    
 }
